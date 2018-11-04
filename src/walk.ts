a21d5674d8ff93dda81a8f5b3ce91d32092b8e46
Jdender~ <jdenderplays@gmail.com>
2018-11-04 14:49:43 -0500

    Export everything in walk.ts for jest

--- src/walk.ts
+++ src/walk.ts
@@ -2,19 +2,19 @@ import { promises as fsp } from 'fs';
 import path = require('path');
 
 // Turn a nested array to a array
-const flattenArray = <T>(arr: T[][]) => arr.reduce((acc, val) => acc.concat(val), []);
+export const flattenArray = <T>(arr: T[][]) => arr.reduce((acc, val) => acc.concat(val), []);
 
 // Short for fs.stat.isDirectory
-const isDirectory = async (dir: string) => (await fsp.stat(dir)).isDirectory();
+export const isDirectory = async (dir: string) => (await fsp.stat(dir)).isDirectory();
 
 // tslint:ignore-next-line no-use-before-declare
-const walkFile = (dir: string) => (file: string) => walk(path.join(dir, file));
+export const walkFile = (dir: string) => (file: string) => walk(path.join(dir, file));
 
 // Reads a dir and returns nested array of files
-const readDir = async (dir: string) => Promise.all((await fsp.readdir(dir)).map(walkFile(dir)));
+export const readDir = async (dir: string) => Promise.all((await fsp.readdir(dir)).map(walkFile(dir)));
 
 // Same as above but a normal array
-const readDirFlat = async (dir: string) => flattenArray(await readDir(dir));
+export const readDirFlat = async (dir: string) => flattenArray(await readDir(dir));
 
 // Walks the file tree
 export const walk = async (dir: string): Promise<string[]> =>
