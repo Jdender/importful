d62a34d336dfe7602e1c2afdf94687e83ebbd7ea
Jdender~ <jdenderplays@gmail.com>
2018-11-04 14:49:55 -0500

    Add importful to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+import { walk } from './walk';
+
+// Return a promise for a array of promises to imported modules
+export const importful = async (dir: string) =>
+    (await walk(dir))
+    .map(file => import(file));
+
+export default importful;
 export * from './walk';
